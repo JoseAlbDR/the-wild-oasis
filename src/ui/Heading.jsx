12070ceade4e7321378d84eaa8b0e420a6d547d8
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-24 16:44:15 +0200

    refactor: refactor diferent types of heading into diferent variables in Heading sylted component

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -1,28 +1,23 @@
 import { css, styled } from "styled-components";
 
+const types = {
+  h1: css`
+    font-size: 3rem;
+    font-weight: 600;
+  `,
+  h2: css`
+    font-size: 2rem;
+    font-weight: 600;
+  `,
+  h3: css`
+    font-size: 2rem;
+    font-weight: 500;
+  `,
+};
+
 const Heading = styled.h1`
-  ${(props) =>
-    props.as === "h1" &&
-    css`
-      font-size: 3rem;
-      font-weight: 600;
-    `}
-
-  ${(props) =>
-    props.as === "h2" &&
-    css`
-      font-size: 2rem;
-      font-weight: 600;
-    `}
-
-  ${(props) =>
-    props.as === "h3" &&
-    css`
-      font-size: 2rem;
-      font-weight: 500;
-    `}
-
-  line-height: 1.4
+  ${(props) => types[props.as]}
+  line-height: 1.4;
 `;
 
 export default Heading;
