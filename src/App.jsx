885127c868167523582fdf473735b4c7e45114ae
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-24 13:13:03 +0200

    feat: install styled-components, build a couple of components to test it

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { styled } from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  cursor: pointer;
+`;
+
 function App() {
-  return "Hello there!";
+  return (
+    <div>
+      <H1>Hello there!</H1>
+      <Button onClick={() => alert("Check in")}>Check In</Button>
+    </div>
+  );
 }
 
 export default App;
