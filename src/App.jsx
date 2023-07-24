ffb00b2844b83c89615b5c610f290b60522bcc24
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-24 13:17:09 +0200

    feat: style a prebuild component with convention name in App component

--- src/App.jsx
+++ src/App.jsx
@@ -14,14 +14,28 @@ const Button = styled.button`
   background-color: purple;
   color: white;
   cursor: pointer;
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
 `;
 
 function App() {
   return (
-    <div>
+    <StyledApp>
       <H1>Hello there!</H1>
-      <Button onClick={() => alert("Check in")}>Check In</Button>
-    </div>
+      <Button onClick={() => alert("Check In")}>Check In</Button>
+      <Button onClick={() => alert("Check Out")}>Check Out</Button>
+      <Input type="number" placeholder="Number of guests" />
+    </StyledApp>
   );
 }
 
