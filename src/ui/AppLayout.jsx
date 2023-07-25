813c5b0f1ef897b4b25d0161dfdd2fc5c306bc23
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-25 10:14:13 +0200

    feat: build AppLayout with Header, Sidebar and Main styled components

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,13 +1,29 @@
 import { Outlet } from "react-router-dom";
 import SideBar from "./SideBar";
 import Header from "./Header";
+import styled from "styled-components";
+
+const StyledAppLayout = styled.div`
+  display: grid;
+  height: 100vh;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+`;
+
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
+
 function AppLayout() {
   return (
-    <div>
-      <SideBar />
+    <StyledAppLayout>
       <Header />
-      <Outlet />
-    </div>
+      <SideBar />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledAppLayout>
   );
 }
 
