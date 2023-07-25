5c114ced5d2614672c9849df7d2efb95e66aad36
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-25 15:58:09 +0200

    feat: create apiCabins service to get all cabins from supabase

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,17 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img
+        src="https://lvjjzbmjjiicloqggvli.supabase.co/storage/v1/object/public/cabins-images/cabin-001.jpg"
+        alt="cabin 1"
+      />
     </Row>
   );
 }
