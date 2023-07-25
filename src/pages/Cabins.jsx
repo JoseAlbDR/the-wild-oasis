c7e6227bcd8e196d5fe3667145cbc394741e5444
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-26 00:43:14 +0200

    feat: Cabins component layout, Fetch data in CabinTable, render a CabinRow for each cabin fetched

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,18 +1,19 @@
-import { useEffect } from "react";
+import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      <img
-        src="https://lvjjzbmjjiicloqggvli.supabase.co/storage/v1/object/public/cabins-images/cabin-001.jpg"
-        alt="cabin 1"
-      />
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
