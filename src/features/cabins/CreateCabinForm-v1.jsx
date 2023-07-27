30cebbf73b5ab821d506e6ab4029b7555ea12987
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-27 10:56:15 +0200

    feat: save old CreateCabinForm version

---
+++ src/features/cabins/CreateCabinForm-v1.jsx
@@ -0,0 +1,141 @@
+import { toast } from "react-hot-toast";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import Input from "../../ui/Input";
+import Form from "../../ui/Form";
+import Button from "../../ui/Button";
+import FileInput from "../../ui/FileInput";
+import Textarea from "../../ui/Textarea";
+import FormRow from "../../ui/FormRow";
+
+import { useForm } from "react-hook-form";
+import { addCabin } from "../../services/apiCabins";
+
+function CreateCabinForm({ cabin }) {
+  const { name, image, maxCapacity, regularPrice, discount } = cabin;
+  // First part get register and handleSubmit from hook
+  const { register, handleSubmit, reset, getValues, formState } = useForm();
+  const { errors } = formState;
+  const queryClient = useQueryClient();
+
+  const { isLoading: isCreating, mutate } = useMutation({
+    mutationFn: addCabin,
+    onSuccess: () => {
+      toast.success("Cabin succesfully created", { position: "top-right" });
+      // To refetch data and rerender component
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"],
+      });
+      reset();
+    },
+    onError: (err) => toast.error(err.message, { position: "top-right" }),
+  });
+
+  // Second part use our own onSubmit
+  function onSubmit(data) {
+    console.log(data);
+    // set image to data.image recieved from form
+    mutate({ ...data, image: data.image[0] });
+  }
+
+  function onError(errors) {
+    console.log(errors);
+  }
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+      <FormRow error={errors?.name?.message} label="Cabin Name">
+        <Input
+          type="text"
+          id="name"
+          disabled={isCreating}
+          {...register("name", {
+            required: "This field is required",
+            min: {
+              value: 1,
+              message: "Capcity shouuld be at least 1.",
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow error={errors?.maxCapacity?.message} label="Maximum capacity">
+        <Input
+          type="number"
+          id="maxCapacity"
+          disabled={isCreating}
+          {...register("maxCapacity", {
+            required: "This field is required",
+          })}
+        />
+      </FormRow>
+
+      <FormRow error={errors?.regularPrice?.message} label="Regular price">
+        <Input
+          type="number"
+          id="regularPrice"
+          disabled={isCreating}
+          {...register("regularPrice", {
+            required: "This field is required",
+            min: {
+              value: 1,
+              message: "Capcity shouuld be at least 1.",
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow error={errors?.discount?.message} label="Discount">
+        <Input
+          type="number"
+          id="discount"
+          disabled={isCreating}
+          defaultValue={0}
+          {...register("discount", {
+            required: "This field is required",
+            validate: (value) =>
+              value <= getValues().regularPrice ||
+              "Discount should be less than regular price",
+          })}
+        />
+      </FormRow>
+
+      <FormRow
+        error={errors?.description?.message}
+        label="Description for cabin"
+      >
+        <Textarea
+          type="number"
+          id="description"
+          disabled={isCreating}
+          defaultValue=""
+          {...register("description", {
+            required: "This field is required",
+          })}
+        />
+      </FormRow>
+
+      <FormRow label="Cabin photo">
+        <FileInput
+          id="image"
+          accept="image/*"
+          // type="file"
+          disabled={isCreating}
+          {...register("image", {
+            required: "This field is required",
+          })}
+        />
+      </FormRow>
+
+      <FormRow>
+        {/* type is an HTML attribute! */}
+        <Button variation="secondary" type="reset">
+          Cancel
+        </Button>
+        <Button disabled={isCreating}>Add cabin</Button>
+      </FormRow>
+    </Form>
+  );
+}
+
+export default CreateCabinForm;
