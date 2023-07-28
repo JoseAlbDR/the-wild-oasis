8f1e124c2866d02a9243063c5c7bcc1d8c41277c
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-28 09:37:45 +0200

    style: minor style changes

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -134,7 +134,6 @@ function CreateCabinForm({ cabinToEdit = {} }) {
         <Textarea
           type="number"
           id="description"
-          disabled={isWorking}
           defaultValue=""
           {...register("description", {
             required: "This field is required",
