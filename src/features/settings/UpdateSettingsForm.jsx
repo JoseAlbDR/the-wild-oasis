1772d4708fb3d26390f789d361185beb0ae9bd40
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-28 11:14:44 +0200

    feat: fetch settings data inside UpdateSettingsForm component

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,22 +1,57 @@
-import Form from ', ../.. /ui/Form';
-import FormRow from '../../ui/FormRow';
-import Input from '../../ui/Input';
+import Form from "../../ui/Form";
+import FormRow from "../../ui/FormRow";
+import Input from "../../ui/Input";
+import { useSettings } from "./useSettings";
 
 function UpdateSettingsForm() {
+  const { isLoading, settings, isError } = useSettings();
+
+  if (!settings) return;
+
+  const {
+    breakfastPrice,
+    maxBookingLength,
+    maxGuestPerCabin,
+    minBookingLength,
+  } = settings;
+
+  function handleUpdate() {}
+
   return (
     <Form>
-      <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
+      <FormRow label="Minimum nights/booking">
+        <Input
+          disabled={isLoading}
+          defaultValue={minBookingLength}
+          type="number"
+          id="min-nights"
+        />
       </FormRow>
-      <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
+      <FormRow label="Maximum nights/booking">
+        <Input
+          disabled={isLoading}
+          defaultValue={maxBookingLength}
+          type="number"
+          id="max-nights"
+        />
       </FormRow>
-      <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
+      <FormRow label="Maximum guests/booking">
+        <Input
+          disabled={isLoading}
+          defaultValue={maxGuestPerCabin}
+          type="number"
+          id="max-guests"
+        />
       </FormRow>
-      <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
+      <FormRow label="Breakfast price">
+        <Input
+          disabled={isLoading}
+          defaultValue={breakfastPrice}
+          type="number"
+          id="breakfast-price"
+        />
       </FormRow>
+      <button onClick={handleUpdate}>Update</button>
     </Form>
   );
 }
