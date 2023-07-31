98af2c7c633fdb8a20dda0316086831fb6400395
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-31 22:15:37 +0200

    feat: implement Modal logic wich renders a Modal window with the CreateCabinForm (children) inside

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,3 +1,6 @@
+import { ClickAwayListener } from "@mui/material";
+import { createPortal } from "react-dom";
+import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
 
 const StyledModal = styled.div`
@@ -48,3 +51,21 @@ const Button = styled.button`
     color: var(--color-grey-500);
   }
 `;
+
+function Modal({ children, onClose }) {
+  return createPortal(
+    <Overlay>
+      <ClickAwayListener onClickAway={onClose}>
+        <StyledModal>
+          <Button onClick={onClose}>
+            <HiXMark />
+          </Button>
+          <div>{children}</div>
+        </StyledModal>
+      </ClickAwayListener>
+    </Overlay>,
+    document.body
+  );
+}
+
+export default Modal;
