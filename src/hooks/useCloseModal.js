6adb012c76bd0b91a3186aa333e53e96b06589ea
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-01 08:53:00 +0200

    feat: new custom hook useCloseModal to close the modal when clicking outside it

---
+++ src/hooks/useCloseModal.js
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from "react";
+
+export function useCloseModal(handler, listenCapturing = true) {
+  const modalRef = useRef();
+
+  useEffect(
+    function () {
+      function handleClick(e) {
+        if (modalRef.current && !modalRef.current.contains(e.target)) handler();
+      }
+
+      document.addEventListener("click", handleClick, listenCapturing);
+
+      return () =>
+        document.removeEventListener("click", handleClick, listenCapturing);
+    },
+    [handler, listenCapturing]
+  );
+
+  return modalRef;
+}
