deea0964d8c36150d470d7855a1b939a6af38e59
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-01 16:07:39 +0200

    feat: create DeleteCabin modal

---
+++ src/features/cabins/DeleteCabin.jsx
@@ -0,0 +1,24 @@
+import { HiTrash } from "react-icons/hi2";
+import Modal from "../../ui/Modal";
+import ConfirmDelete from "../../ui/ConfirmDelete";
+
+function DeleteCabin({ name, onConfirm, isDeleting }) {
+  return (
+    <>
+      <Modal.Open opens="delete">
+        <button>
+          <HiTrash />
+        </button>
+      </Modal.Open>
+      <Modal.Window name="delete">
+        <ConfirmDelete
+          resourceName={name}
+          onConfirm={onConfirm}
+          disabled={isDeleting}
+        />
+      </Modal.Window>
+    </>
+  );
+}
+
+export default DeleteCabin;
