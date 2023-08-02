40f86ca4b2fb46f9ee3758a6e31d18dec729c29f
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-03 00:06:59 +0200

    feat: new component SortBy that recieves an array of options to show

---
+++ src/ui/SortBy.jsx
@@ -0,0 +1,20 @@
+import Select from "./Select";
+import { useUrl } from "../hooks/useUrl";
+
+function SortBy({ options }) {
+  const { setUrl, getUrl } = useUrl("sortBy", "");
+  function handleChange(value) {
+    setUrl(value);
+  }
+
+  return (
+    <Select
+      options={options}
+      value={getUrl}
+      type="white"
+      onChange={(e) => handleChange(e.target.value)}
+    />
+  );
+}
+
+export default SortBy;
