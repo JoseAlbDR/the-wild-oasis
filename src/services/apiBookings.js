2123358d5e9892f4ddd7d31999fdee412c8e89c0
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-03 22:31:04 +0200

    feat: add paginatio query to apiBookings

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,10 +1,11 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
+import { PAGE_SIZE } from "../utils/constants";
 
-export async function getBookings({ filter, sortBy }) {
+export async function getBookings({ filter, sortBy, page }) {
   let query = supabase
     .from("bookings")
-    .select("*, cabins(name), guests(fullName, email)");
+    .select("*, cabins(name), guests(fullName, email)", { count: "exact" });
 
   // FILTER
   // We can add any filter we want if filter is an array of filters and we map then to query
@@ -16,13 +17,21 @@ export async function getBookings({ filter, sortBy }) {
       ascending: sortBy.direction === "asc",
     });
 
-  const { data, error } = await query;
+  // PAGINATION
+  if (page) {
+    const from = (page - 1) * PAGE_SIZE;
+    const to = page * PAGE_SIZE - 1;
+    query = query.range(from, to);
+  }
+
+  const { data, error, count } = await query;
+
   if (error) {
     console.error(error);
     throw new Error("Bookings could not be retrieved.");
   }
 
-  return data;
+  return { data, count };
 }
 
 export async function getBooking(id) {
