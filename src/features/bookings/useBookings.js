3fd184e98477b7ba4f15bfb2d5f0e846bbb2a709
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-03 17:09:32 +0200

    feat: pass filter to getBookins queryFn based on searchParams status

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export const useBookings = () => {
+  const [searchParams] = useSearchParams();
+
+  // FILTER
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : // Instead of passing 1 object we can pass an array of objects to add more filters
+        { field: "status", value: filterValue, method: "eq" };
+
   const {
     isLoading,
     data: bookings,
     error: isError,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    // Add filter, so whenever filter changes react query will refetch the data AWESOME
+    // DEPENDENCY ARRAY LIKE USE EFFECT
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter }),
   });
 
   return { isLoading, bookings, isError };
