d2e94f57d5975c5330d85a8b48e98c6b235c6cf6
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-04 18:52:54 +0200

    feat: custom hook to get one booking given the id

---
+++ src/features/bookings/useBooking.js
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import { getBooking } from "../../services/apiBookings";
+import { useParams } from "react-router-dom";
+
+export const useBooking = () => {
+  const { bookingId } = useParams();
+  const {
+    isLoading,
+    data: booking,
+    error: isError,
+  } = useQuery({
+    // Identifies each data
+    queryKey: ["bookings"],
+    queryFn: () => getBooking(bookingId),
+    retry: false,
+  });
+  return {
+    isLoading,
+    booking,
+    isError,
+  };
+};
