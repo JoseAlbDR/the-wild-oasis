4d8641db66a719dc0b017b7818ada48ab77c8f92
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-05 10:33:28 +0200

    feat: add bookingId to useBooking dependency array

--- src/features/bookings/useBooking.js
+++ src/features/bookings/useBooking.js
@@ -10,7 +10,7 @@ export const useBooking = () => {
     error: isError,
   } = useQuery({
     // Identifies each data
-    queryKey: ["booking"],
+    queryKey: ["booking", bookingId],
     queryFn: () => getBooking(bookingId),
     retry: false,
   });
