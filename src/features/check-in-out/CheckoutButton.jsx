39243b85452fc11fe6c453264c13b5642e814158
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-05 16:07:14 +0200

    feat: add handleCheckout onClick even handle and useCheckout custom hook to handle a booking checkout in CheckoutButton component

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,8 +1,21 @@
 import Button from "../../ui/Button";
+import Spinner from "../../ui/Spinner";
+import { useCheckout } from "./useCheckout";
 
 function CheckoutButton({ bookingId }) {
+  const { isCheckingOut, checkOutBooking } = useCheckout();
+
+  function handleCheckout() {
+    checkOutBooking(bookingId);
+  }
+
   return (
-    <Button variation="primary" size="small">
+    <Button
+      disabled={isCheckingOut}
+      onClick={handleCheckout}
+      variation="primary"
+      size="small"
+    >
       Check out
     </Button>
   );
