d2c6ed02eb24ffd7e919e3504495a9318198874e
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-06 00:11:07 +0200

    feat: getCurrentUser function to get user from session

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -11,12 +11,13 @@ export async function login({ email, password }) {
 }
 
 export async function getCurrentUser() {
-  const { data: session } = await supabase.auth.getSession();
-  if (!session.session) return null;
+  const {
+    data: { session: session },
+  } = await supabase.auth.getSession();
+
+  if (!session) return null;
 
   const { data, error } = await supabase.auth.getUser();
-
-  console.log(data);
 
   if (error) throw new Error(error.message);
 
