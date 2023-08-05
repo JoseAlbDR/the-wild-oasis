90d16e09034e429aedf730051c54a5179d4a95a1
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-05 18:25:00 +0200

    feat: useDeleteBooking custom hook to delelete a booking given the id

---
+++ src/features/bookings/useDeleteBooking.js
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteBooking as deleteBookingApi } from "../../services/apiBookings";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+export const useDeleteBooking = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const { isLoading: isDeleting, mutate: deleteBooking } = useMutation({
+    mutationFn: deleteBookingApi,
+    onSuccess: (data) => {
+      toast.success(`Booking ${data.id} successfully deleted`, {
+        position: "top-right",
+      });
+      queryClient.invalidateQueries({
+        queryKey: ["bookings", "booking"],
+      });
+      navigate("/bookings");
+    },
+    onError: (error) => toast.error(error, { position: "top-right" }),
+  });
+
+  return { isDeleting, deleteBooking };
+};
