824a983d9f2763b5c02fb80fcc324194d79b7062
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-05 16:01:45 +0200

    feat: custom hook to checkout a booking

---
+++ src/features/check-in-out/useCheckout.js
@@ -0,0 +1,30 @@
+import {
+  QueryClient,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { updateBooking } from "../../services/apiBookings";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+export const useCheckout = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const { isLoading: isCheckingOut, mutate: checkOutBooking } = useMutation({
+    mutationFn: (bookingId) =>
+      updateBooking(bookingId, { status: "checked-out" }),
+    onSuccess: (data) => {
+      toast.success(`Booking ${data.id} successfully checket out`, {
+        position: "top-right",
+      }),
+        queryClient.invalidateQueries({
+          active: true,
+        }),
+        navigate("/");
+    },
+    onError: (err) => toast.error(err.message, { position: "top-right" }),
+  });
+
+  return { isCheckingOut, checkOutBooking };
+};
