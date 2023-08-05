0f1ed199f17ad5c6cfbf6657d23e36c30d9abedc
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-05 16:04:09 +0200

    feat: add a check out option in the context menu of BookingRow, useCheckout hook to handle onClick event on that button

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -1,13 +1,15 @@
 import styled from "styled-components";
 import { format, isToday } from "date-fns";
 import { useNavigate } from "react-router-dom";
-import { HiArrowDownOnSquare, HiEye } from "react-icons/hi2";
+import { HiArrowDownOnSquare, HiArrowUpOnSquare, HiEye } from "react-icons/hi2";
 
 import Tag from "../../ui/Tag";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { formatCurrency } from "../../utils/helpers";
 import { formatDistanceFromNow } from "../../utils/helpers";
+import CheckoutButton from "../check-in-out/CheckoutButton";
+import { useCheckout } from "../check-in-out/useCheckout";
 
 const Cabin = styled.div`
   font-size: 1.6rem;
@@ -51,6 +53,7 @@ function BookingRow({
   },
 }) {
   const navigate = useNavigate();
+  const { isCheckingOut, checkOutBooking } = useCheckout();
 
   const statusToTagName = {
     unconfirmed: "blue",
@@ -99,6 +102,16 @@ function BookingRow({
               onClick={() => navigate(`/checkin/${bookingId}`)}
             >
               Check in
+            </Menus.Button>
+          )}
+          {status === "checked-in" && (
+            <Menus.Button
+              icon={<HiArrowUpOnSquare />}
+              onClick={() => {
+                checkOutBooking(bookingId);
+              }}
+            >
+              Check out
             </Menus.Button>
           )}
         </Menus.List>
