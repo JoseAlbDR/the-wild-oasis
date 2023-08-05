2af18ecdea483144b76d84be7c5f5a14e353025c
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-06 00:09:04 +0200

    feat: add logic to ProtectedRoute to redirect to Dashboard if there is an user authenticated or to Login page if not

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,5 +1,39 @@
+import { useNavigate } from "react-router-dom";
+import { useGetUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import { styled } from "styled-components";
+import { useEffect } from "react";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 function ProtectedRoute({ children }) {
-  return { children };
+  const navigate = useNavigate();
+
+  // 1. Load the authenticated user
+  const { isLoadingUser, isAuthenticated } = useGetUser();
+
+  // 2. If there is NO authenticaded user, redirect to the /login
+  //
+  useEffect(() => {
+    if (!isAuthenticated && !isLoadingUser) navigate("/login");
+  }, [isAuthenticated, navigate, isLoadingUser]);
+
+  // 3. WHile loading, show a spinner
+  if (isLoadingUser)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  // 4. If there is a user, render the app
+  if (isAuthenticated) return children;
 }
 
 export default ProtectedRoute;
