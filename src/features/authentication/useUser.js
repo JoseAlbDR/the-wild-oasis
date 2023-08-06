d81e98c0b1095c764b5a07c304ba73931843b376
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-06 19:02:39 +0200

    refactor: change useGetUser to useUser

--- src/features/authentication/useUser.js
+++ src/features/authentication/useUser.js
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { getCurrentUser } from "../../services/apiAuth";
 
-export const useGetUser = () => {
+export const useUser = () => {
   const { isLoading: isLoadingUser, data: user } = useQuery({
     queryKey: ["user"],
     queryFn: getCurrentUser,
