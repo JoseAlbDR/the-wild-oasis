e6a9bb041744aa6e900692bea4fdbf860e3265f3
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-06 19:48:17 +0200

    feat: add new service updateCurrentUser

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,5 +1,5 @@
 import supabase from "./supabase";
-
+import { supabaseUrl, supabaseAvatarsBucketUrl } from "./supabase";
 export async function signup({ fullName, email, password }) {
   const { data, error } = await supabase.auth.signUp({
     email,
@@ -42,4 +42,39 @@ export async function logout() {
   const { error } = await supabase.auth.signOut();
 
   if (error) throw new Error(error.message);
+}
+
+export async function updateCurrentUser({ password, newFullName, newAvatar }) {
+  // 1 Update fullName || password
+  let updateData;
+  if (password) updateData = { password };
+  if (newFullName) updateData = { data: { newFullName } };
+
+  const { data, error } = await supabase.auth.updateUser({
+    updateData,
+  });
+
+  if (error) throw new Error(error.message);
+  if (!newAvatar) return data;
+
+  // 2 Update avatar
+  const avatarName = `avatar-${data.user.id}-${Math.random()}`;
+
+  const { error: avatarError } = await supabase.storage
+    .from("avatars")
+    .upload(avatarName, newAvatar);
+
+  if (avatarError) throw new Error(avatarError.message);
+
+  // 3 Update avatar in the user
+  const { data: updatedUser, error: updatedUserError } =
+    supabase.auth.updateUser({
+      data: {
+        avatar: `${supabaseUrl}/${supabaseAvatarsBucketUrl}/${avatarName}`,
+      },
+    });
+
+  if (updatedUserError) throw new Error(updatedUserError.message);
+
+  return updatedUser;
 }
