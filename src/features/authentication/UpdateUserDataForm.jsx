cf766b3923020c27cb58459deab35955024390b5
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-07 00:40:43 +0200

    feat: implement cancel button logic in UpdateUserDataForm

--- src/features/authentication/UpdateUserDataForm.jsx
+++ src/features/authentication/UpdateUserDataForm.jsx
@@ -27,7 +27,20 @@ function UpdateUserDataForm() {
   function handleSubmit(e) {
     e.preventDefault();
     if (!fullName) return;
-    updateUser({ fullName, avatar });
+    updateUser(
+      { fullName, avatar },
+      {
+        onSuccess: () => {
+          setAvatar(null);
+          e.target.reset();
+        },
+      }
+    );
+  }
+
+  function handleCancel() {
+    setFullName(currentFullName);
+    setAvatar(null);
   }
 
   return (
@@ -55,7 +68,12 @@ function UpdateUserDataForm() {
       </FormRow>
 
       <FormRow>
-        <Button type="reset" variation="secondary">
+        <Button
+          type="reset"
+          variation="secondary"
+          disabled={isUpdating}
+          onClick={handleCancel}
+        >
           Cancel
         </Button>
         {isUpdating ? <SpinnerMini /> : <Button>Update account</Button>}
