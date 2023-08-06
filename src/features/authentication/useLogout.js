b49e24ebf34f4dbb9f6b06172b418e8d2a6c5171
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-06 08:34:51 +0200

    feat: useLogout custom hook with a mutation

---
+++ src/features/authentication/useLogout.js
@@ -0,0 +1,19 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { logout as logoutApi } from "../../services/apiAuth";
+import { toast } from "react-hot-toast";
+
+export const useLogout = () => {
+  const queryClient = useQueryClient();
+
+  const { isLoading: isLogingOut, mutate: logout } = useMutation({
+    mutationFn: logoutApi,
+    onSuccess: () => {
+      toast.success("Successfully logged out", { position: "top-right" });
+      queryClient.invalidateQueries({
+        queryKey: ["user"],
+      });
+    },
+  });
+
+  return { isLogingOut, logout };
+};
