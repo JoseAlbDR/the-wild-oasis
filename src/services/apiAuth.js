b4f54d82890c85cee3655c06daa22f18bac7af91
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-06 08:36:37 +0200

    feat: logout function inside apiAuth

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -6,7 +6,6 @@ export async function login({ email, password }) {
   });
 
   if (error) throw new Error(error.message);
-
   return data;
 }
 
@@ -14,12 +13,16 @@ export async function getCurrentUser() {
   const {
     data: { session: session },
   } = await supabase.auth.getSession();
-
   if (!session) return null;
 
   const { data, error } = await supabase.auth.getUser();
 
   if (error) throw new Error(error.message);
-
   return data?.user;
+}
+
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+
+  if (error) throw new Error(error.message);
 }
