4594a4e42cc49fc168aaebeafa12ce7f952242f2
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-06 19:45:14 +0200

    refactor: change VITE_SUPABASE_BUCKET_URl to VITE_SUPABASE_CABINS_BUCKET_URL feat: add VITE_SUPABASE_AVATARS_BUCKET_URL env variable

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-export const supabaseBucketUrl = import.meta.env.VITE_SUPABASE_BUCKET_URL;
+export const supabaseCabinsBucketUrl = import.meta.env
+  .VITE_SUPABASE_CABINS_BUCKET_URL;
+export const supabaseAvatarsBucketUrl = import.meta.env
+  .VITE_SUPABASE_AVATARS_BUCKET_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
