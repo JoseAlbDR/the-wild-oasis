e823232beaea6fce7365e5819bbd928029c7b100
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-06 22:08:07 +0200

    feat: hande submit in UpdateUserDataForm along with spinner when loading

--- src/features/authentication/UpdateUserDataForm.jsx
+++ src/features/authentication/UpdateUserDataForm.jsx
@@ -7,6 +7,8 @@ import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 
 import { useUser } from "./useUser";
+import { useUpdateUser } from "./useUpdateUser";
+import SpinnerMini from "../../ui/SpinnerMini";
 
 function UpdateUserDataForm() {
   // We don't need the loading state, and can immediately use the user data, because we know that it has already been loaded at this point
@@ -17,11 +19,15 @@ function UpdateUserDataForm() {
     },
   } = useUser();
 
+  const { isUpdating, updateUser } = useUpdateUser();
+
   const [fullName, setFullName] = useState(currentFullName);
   const [avatar, setAvatar] = useState(null);
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!fullName) return;
+    updateUser({ fullName, avatar });
   }
 
   return (
@@ -29,14 +35,17 @@ function UpdateUserDataForm() {
       <FormRow label="Email address">
         <Input value={email} disabled />
       </FormRow>
+
       <FormRow label="Full name">
         <Input
           type="text"
           value={fullName}
           onChange={(e) => setFullName(e.target.value)}
+          disabled={isUpdating}
           id="fullName"
         />
       </FormRow>
+
       <FormRow label="Avatar image">
         <FileInput
           id="avatar"
@@ -44,11 +53,12 @@ function UpdateUserDataForm() {
           onChange={(e) => setAvatar(e.target.files[0])}
         />
       </FormRow>
+
       <FormRow>
         <Button type="reset" variation="secondary">
           Cancel
         </Button>
-        <Button>Update account</Button>
+        {isUpdating ? <SpinnerMini /> : <Button>Update account</Button>}
       </FormRow>
     </Form>
   );
