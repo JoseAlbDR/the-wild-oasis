f5a04081045faaea3a51ecad8ec74bbf8abad2b6
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-06 08:43:20 +0200

    feat: navigate to /login whenever a user is logged out

--- src/features/authentication/useLogout.js
+++ src/features/authentication/useLogout.js
@@ -1,13 +1,16 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { logout as logoutApi } from "../../services/apiAuth";
 import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 export const useLogout = () => {
   const queryClient = useQueryClient();
+  const navigate = useNavigate();
 
   const { isLoading: isLogingOut, mutate: logout } = useMutation({
     mutationFn: logoutApi,
     onSuccess: () => {
+      navigate("/login", { replace: true });
       toast.success("Successfully logged out", { position: "top-right" });
       queryClient.removeQueries();
     },
