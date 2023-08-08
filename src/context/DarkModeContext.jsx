398072c186226bb9afc964c17712932cf69a9e32
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-08 16:41:08 +0200

    feat: add Dark Mode context and hook to consume it

---
+++ src/context/DarkModeContext.jsx
@@ -0,0 +1,28 @@
+import { createContext, useContext } from "react";
+import { useLocalStorageState } from "../hooks/useLocalStorageState";
+
+const DarkModeContext = createContext();
+
+function DarkModeProvider({ children }) {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+
+  function toggleDarkMode() {
+    setIsDarkMode((isDark) => !isDark);
+  }
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+}
+
+function useDarkMode() {
+  const context = useContext(DarkModeContext);
+
+  if (context === undefined)
+    return "DarkModeContext was used outside of DarKModeProvider";
+  return context;
+}
+
+export { DarkModeProvider, useDarkMode };
